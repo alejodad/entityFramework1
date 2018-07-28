925b79089f0257fe84035584a03a7101d1c25b0b
Alejo <dasierra93@misena.edu.co>
2018-07-27 22:31:48 -0500

    se confirma todo

---
+++ ProyectoMVC1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ProyectoMVC1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dgvDatosProfe = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbMaterias = new System.Windows.Forms.ComboBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatosProfe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(72, 194);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // dgvDatosProfe
+            // 
+            this.dgvDatosProfe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatosProfe.Location = new System.Drawing.Point(72, 235);
+            this.dgvDatosProfe.Name = "dgvDatosProfe";
+            this.dgvDatosProfe.Size = new System.Drawing.Size(240, 150);
+            this.dgvDatosProfe.TabIndex = 1;
+            this.dgvDatosProfe.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDatosProfe_CellDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(72, 92);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 2;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(72, 130);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(72, 168);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Apellido";
+            // 
+            // cmbMaterias
+            // 
+            this.cmbMaterias.FormattingEnabled = true;
+            this.cmbMaterias.Location = new System.Drawing.Point(205, 168);
+            this.cmbMaterias.Name = "cmbMaterias";
+            this.cmbMaterias.Size = new System.Drawing.Size(121, 21);
+            this.cmbMaterias.TabIndex = 8;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(178, 92);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(153, 194);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 10;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(234, 195);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 432);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.cmbMaterias);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvDatosProfe);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatosProfe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dgvDatosProfe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbMaterias;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ ProyectoMVC1/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoMVC1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnModificar.Visible = false;
+            listarProfe();
+            llenaCmb();
+        }
+        MaestrosEntities db = new MaestrosEntities();
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if(txtId.Text != "")
+            {
+                tbl_profesor profe = new tbl_profesor();
+                profe.idProfesor = Convert.ToInt32(txtId.Text);
+                profe.nombreProfesor = txtNombre.Text;
+                profe.apellidoProfesor = txtApellido.Text;
+                addProfe(profe);
+                listarProfe();
+                limpiar();
+            }
+            else
+            {
+                MessageBox.Show("Debe llenar los campos");
+            }
+
+
+        }
+
+        private void addProfe(tbl_profesor profe)
+        {
+            using (MaestrosEntities bd = new MaestrosEntities())
+            {
+                tbl_profesor newProfe = bd.tbl_profesor.Find(profe.idProfesor);
+                if(newProfe != null)
+                {
+                    MessageBox.Show("El profesor ya existe");
+                }
+                else
+                {
+                    newProfe = profe;
+                    bd.tbl_profesor.Add(newProfe);
+                    db.SaveChanges();
+                    MessageBox.Show("Se hizo");
+                }
+                
+            }
+        }
+
+        private void listarProfe()
+        {
+
+            dgvDatosProfe.DataSource = db.tbl_profesor.ToList();
+        }
+
+        private void llenaCmb()
+        {
+            cmbMaterias.DataSource = db.tbl_materia.ToList();
+            cmbMaterias.DisplayMember = "nombreMateria";
+            cmbMaterias.ValueMember = "idMateria";
+        }
+
+        private void buscarProfe()
+        {
+            tbl_profesor profe = new tbl_profesor();
+            profe = db.tbl_profesor.Find(Convert.ToInt32(txtId.Text));
+            if (profe != null)
+            {
+                txtNombre.Text = profe.nombreProfesor;
+                txtApellido.Text = profe.apellidoProfesor;
+                if (profe != null)
+                {
+                    btnModificar.Visible = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se encontraron coincidencias");
+            }
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            buscarProfe();
+        }
+
+        private void modificar(int id)
+        {
+            tbl_profesor profe = new tbl_profesor();
+            profe = db.tbl_profesor.Find(id);
+            profe.nombreProfesor = txtNombre.Text;
+            profe.apellidoProfesor = txtApellido.Text;
+            db.SaveChanges();
+        }
+
+
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            modificar(Convert.ToInt32(txtId.Text));
+            listarProfe();
+            btnModificar.Visible = false;
+            limpiar();
+        }
+        private void limpiar()
+        {
+            txtId.Clear();
+            txtNombre.Clear();
+            txtApellido.Clear();
+        }
+
+
+        private void eliminar(int id)
+        {
+            tbl_profesor profe = new tbl_profesor();
+            profe = db.tbl_profesor.Find(id);
+            db.tbl_profesor.Remove(profe);
+            db.SaveChanges();
+            listarProfe();
+            limpiar();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Seguro que dese eliminar?", "Eliminar", MessageBoxButtons.YesNoCancel);
+
+            if (result == DialogResult.Yes)
+            {
+                eliminar(Convert.ToInt32(txtId.Text));
+            }
+            else if (result == DialogResult.No)
+            {
+            }
+            else if (result == DialogResult.Cancel)
+            {
+            }
+        }
+
+
+        private void dgvDatosProfe_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtId.Text = dgvDatosProfe.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtNombre.Text = dgvDatosProfe.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtApellido.Text = dgvDatosProfe.Rows[e.RowIndex].Cells[2].Value.ToString();
+            btnModificar.Visible = true;
+        }
+    }
+}
